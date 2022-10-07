933e50a8a389c8c63b8783dcc0a1b8d200b5ebea
Petri Sydänmaa <petri.sydanmaa@gmail.com>
2022-10-07 12:07:58 +0300

    Set up timer

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,31 @@
 
 use panic_halt as _;
 use cortex_m_rt::entry;
+use cortex_m::Peripherals;
+use cortex_m::peripheral::SYST;
 use cortex_m_semihosting::{debug, hprintln};
 
 #[entry]
 fn main() -> ! {
-    hprintln!("Hello World!!!");
+    let peripherals = Peripherals::take().unwrap();
+    
+    // Set up the timer
+    let mut counter = peripherals.SYST;
+    counter.set_reload(0xff_ffff);
+    counter.clear_current();
+    counter.enable_counter();
+ 
+    // Print config info about the timer
+    hprintln!("Is counter   enabled: {}",   counter.is_counter_enabled());
+    hprintln!("Is interrupt enabled: {}",   counter.is_interrupt_enabled());
+    hprintln!("Clk source          : {:?}", counter.get_clock_source());
+    hprintln!("Is precise          : {}", SYST::is_precise());
+    hprintln!("Has ref clock       : {}", SYST::has_reference_clock());
+
+    // Print clock info
+    hprintln!("Wrapped? {}", counter.has_wrapped());
+    hprintln!("Current? {:x}", SYST::get_current());
+
     debug::exit(debug::EXIT_SUCCESS);
     loop {}
 }
