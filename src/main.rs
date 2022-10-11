257a9f560bb9d0cad4901858fd4ce415fda2cb97
Petri Sydänmaa <petri.sydanmaa@gmail.com>
2022-10-11 12:59:50 +0300

    Set interrupt interval

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@
 use panic_halt as _;
 use cortex_m_rt::{entry, exception};
 use cortex_m::Peripherals;
-use cortex_m::peripheral::SYST;
+use cortex_m::peripheral::{SYST, DWT, DCB};
 use cortex_m_semihosting::{debug, hprintln};
 use cortex_m::asm::wfi;
 
@@ -12,36 +12,30 @@ use cortex_m::asm::wfi;
 fn main() -> ! {
     let peripherals = Peripherals::take().unwrap();
     
-    // Set up the timer
+    // Set up the timer to interrupt every 10ms
     let mut counter = peripherals.SYST;
-    counter.set_reload(0x0f_ffff);
+    counter.set_reload(SYST::get_ticks_per_10ms());
     counter.clear_current();
     counter.enable_interrupt();
     counter.enable_counter();
  
-    // Print config info about the timer
-    hprintln!("Is counter   enabled: {}",   counter.is_counter_enabled());
-    hprintln!("Is interrupt enabled: {}",   counter.is_interrupt_enabled());
-    hprintln!("Clk source          : {:?}", counter.get_clock_source());
-    hprintln!("Is precise          : {}", SYST::is_precise());
-    hprintln!("Has ref clock       : {}", SYST::has_reference_clock());
-    hprintln!("");
-
-    // Print clock info
-    hprintln!("Wrapped? {}", counter.has_wrapped());
-    hprintln!("Current? {:x}", SYST::get_current());
-    hprintln!("");
-
     // Setting up the system control block
     let mut scb = peripherals.SCB;
     scb.set_sleepdeep();
     hprintln!("Wait for interrupt");
     wfi();  // Sleep
 
-    // Print clock info
-    hprintln!("Wrapped? {}", counter.has_wrapped());
-    hprintln!("Current? {:x}", SYST::get_current());
-    hprintln!("");
+    // Enable debugger trace
+    let mut dcb = peripherals.DCB;
+    dcb.enable_trace();
+
+    // Get dwt
+    let mut dwt = peripherals.DWT;
+    DWT::unlock();
+    dwt.enable_cycle_counter();
+    hprintln!("Cycle counter enabled: {}", DWT::cycle_counter_enabled());
+    
+    
 
 
     debug::exit(debug::EXIT_SUCCESS);
