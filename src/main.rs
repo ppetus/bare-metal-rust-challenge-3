0f89a12fc3c20f347d19d5a6247152f559cc0498
Petri Sydänmaa <petri.sydanmaa@gmail.com>
2022-10-12 14:16:55 +0300

    Wrote the main program for the challenge

--- src/main.rs
+++ src/main.rs
@@ -4,46 +4,56 @@
 use panic_halt as _;
 use cortex_m_rt::{entry, exception};
 use cortex_m::Peripherals;
-use cortex_m::peripheral::{SYST, DWT, DCB};
-use cortex_m_semihosting::{debug, hprintln};
+use cortex_m::peripheral::SYST;
+use cortex_m_semihosting::hprintln;
 use cortex_m::asm::wfi;
+
+static mut NUM_INTERRUPTS: i32 = 0;
 
 #[entry]
 fn main() -> ! {
-    let peripherals = Peripherals::take().unwrap();
+   let peripherals = Peripherals::take().unwrap();
     
-    // Set up the timer to interrupt every 10ms
+    // Set up the timer to interrupt every 10ms in the beginning
     let mut counter = peripherals.SYST;
-    counter.set_reload(SYST::get_ticks_per_10ms());
+    let mut counter_reload_value = SYST::get_ticks_per_10ms();
+    counter.set_reload(counter_reload_value);
     counter.clear_current();
     counter.enable_interrupt();
     counter.enable_counter();
  
-    // Setting up the system control block
+    // Setting up the system control block to set the device to deep sleep between interrupts
     let mut scb = peripherals.SCB;
     scb.set_sleepdeep();
-    hprintln!("Wait for interrupt");
-    wfi();  // Sleep
 
-    // Enable debugger trace
-    let mut dcb = peripherals.DCB;
-    dcb.enable_trace();
+    // Local variable to minimize the amount of lines inside unsafe
+    let mut local_num_interrupts;
+    loop {
+        hprintln!("Going to sleep");
+        wfi();
+        unsafe { local_num_interrupts = NUM_INTERRUPTS; }
+        
+        // Increse the interval of interrupts after ten interrupts
+        if local_num_interrupts >= 10 {
+            counter.disable_interrupt();  // Do not want to get interrupted heres
+            hprintln!("\n10 interrupts occurred. Raising the timer reload value and resetting the number of occurred interrupts\n");
+            counter_reload_value += SYST::get_reload();  // Basically overly complicated way to double the value :)
+            counter.set_reload(counter_reload_value);
+            counter.clear_current();
 
-    // Get dwt
-    let mut dwt = peripherals.DWT;
-    DWT::unlock();
-    dwt.enable_cycle_counter();
-    hprintln!("Cycle counter enabled: {}", DWT::cycle_counter_enabled());
-    
-    
+            local_num_interrupts = 0;
+            unsafe {NUM_INTERRUPTS = 0;}
 
-
-    debug::exit(debug::EXIT_SUCCESS);
-    loop {}
+            counter.enable_interrupt();  // Continue the program execution
+        }
+    }
 }
 
+// Interrupt handler to record the number of occurred timer interrupts
 #[exception]
 fn SysTick() {
-    hprintln!("Interrupt happened");
-    hprintln!("");
+    unsafe {
+        NUM_INTERRUPTS += 1;
+        hprintln!("New interrupt occurred! Number of occurred interrupts: {}", NUM_INTERRUPTS);
+    }
 }
